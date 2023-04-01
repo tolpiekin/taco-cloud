40da3fef6d54879fcf346c2528767bbc384cdd5e
Volodymyr Tolpiekin <tolpekin.vladimir@gmail.com>
2023-04-01 17:11:34 +0300

    Added data validation.

--- src/main/java/dev/tolpekin/tacos/Taco.java
+++ src/main/java/dev/tolpekin/tacos/Taco.java
@@ -2,12 +2,18 @@ package dev.tolpekin.tacos;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Taco {
-	
+
+	@NotNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
 	private String name;
+	@NotNull
+	@Size(min = 1, message = "You must choose at least one ingredient")
 	private List<Ingredient> ingredients;
 
 }
--- src/main/java/dev/tolpekin/tacos/web/DesignTacoController.java
+++ src/main/java/dev/tolpekin/tacos/web/DesignTacoController.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import dev.tolpekin.tacos.Ingredient;
@@ -64,9 +66,15 @@ public class DesignTacoController {
 	 }
 
 	@PostMapping
-	public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+	public String processTaco(@Valid Taco taco, Errors errors, @ModelAttribute TacoOrder tacoOrder) {
+
+		if (errors.hasErrors()) {
+			return "design";
+		}
+
 		tacoOrder.addTaco(taco);
 		log.info("Processing taco: {}", taco);
+
 		return "redirect:/orders/current";
 	}
 }
